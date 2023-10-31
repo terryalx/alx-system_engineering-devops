e3e4147ba0b6ed4b49cfdbf149a5e9485933ce7b
setalxswe <setalxswe@gmail.com>
2023-10-31 14:40:08 -0700

    reg

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
+# This script outputs script should output: [SENDER],[RECEIVER],[FLAGS]
+
 input = ARGV[0]
-matches = input.scan(/from:(\w+)|to:(\w+)|flags:([0-9:-]+)/)
+matches = input.scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\]\s\[flags:(\S*)\]/)
 
 formatted_matches = matches.map do |match|
-  match.compact.join(':')
+  match.join(',')
 end
 
 puts formatted_matches.join(',')
