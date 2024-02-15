978388e272c60cddb4eeaeb5ceee44c3c53052c0
Efe-Terry <info.techbro.set@gmail.com>
2024-02-15 09:02:23 +0100

    __UpDated__

--- 0x16-api_advanced/ps.sh
+++ 0x16-api_advanced/ps.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Set your username and password here
-username="terryAlx"
-password="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"
-
-# Git commands
 git add .
-git commit -m "web_scraping"
-
-# Providing credentials to git using environment variables
-# Note: This method isn't secure as it leaves the password visible in process listings
-GIT_ASKPASS=echo GIT_USERNAME="$username" GIT_PASSWORD="$password" git push
-
+git commit -m "__UpDated__"
+git push
