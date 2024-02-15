0241004e3d6184a015f3b648b6115e608d363070
Efe-Terry <info.techbro.set@gmail.com>
2024-02-15 09:09:20 +0100

    __UpDated__

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
+import requests
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """query a subreddit and retrive no of subscribers"""
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
