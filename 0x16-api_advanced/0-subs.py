a1c36f23fc19ed46b99635757a08749a63ee8974
Efe-Terry <info.techbro.set@gmail.com>
2024-02-15 09:00:19 +0100

    web_scraping

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Quering Reddit"""
-
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    """query a subreddit and retrive no of subscribers"""
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    headers = {'User-Agent': 'My user Agent 1.0'}
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json().get('data', {})
-        sub_count = data.get('subscribers', 0)
-        return sub_count
-    else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
+
+    return sub_info.json().get("data").get("subscribers")
--- 0x16-api_advanced/ps.sh
+++ 0x16-api_advanced/ps.sh
@@ -1,7 +1,14 @@
-sudo chmod *
-
 #!/bin/bash
 
+# Set your username and password here
+username="terryAlx"
+password="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"
+
+# Git commands
 git add .
 git commit -m "web_scraping"
-git push
+
+# Providing credentials to git using environment variables
+# Note: This method isn't secure as it leaves the password visible in process listings
+GIT_ASKPASS=echo GIT_USERNAME="$username" GIT_PASSWORD="$password" git push
+
